1c5529b5bd1321034067edb8eb33a7bee2d641e7
neilsakariya <53134446+neilsakariya@users.noreply.github.com>
2021-01-26 11:16:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// ૐ શ્રી ગણેશાય નમઃ 
+var hits = 0;
+var hitElement = document.querySelector('.hits');
+document.body.onkeyup = function(e) {
+    if (e.keyCode == 32) {
+        addHit();
+    }
+}
+
+var addHit = function() {
+    hits++;
+    renderHits();
+}
+
+var renderHits = function() {
+    hitElement.innerHTML = hits;
+}
+
+var resetHits = function() {
+    hits = 0;
+    renderHits();
+}
